ede9ddba6af187cd83030cf1344aaefb7f9194d3
SimonMarkWhittle <smw2ed@virginia.edu>
2018-02-14 20:30:45 -0500

    fiddled with horse

--- The Great Man Theory/Assets/Scripts/AI Behaviors/IndividualBehaviors/HorseControl.cs
+++ The Great Man Theory/Assets/Scripts/AI Behaviors/IndividualBehaviors/HorseControl.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class HorseControl : MonoBehaviour {
 
-    public FollowPointer pointer;
+    public FollowPointer head;
+    FollowPointer bodyPointer;
 
     Vector2 target;
 
@@ -13,10 +14,12 @@ public class HorseControl : MonoBehaviour {
     Camera cam;
 
     float veloc = 0f;
+    float giddup = 30f;
 
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody2D>();
+        bodyPointer = GetComponent<FollowPointer>();
         cam = Camera.main;
 	}
 	
@@ -24,29 +27,37 @@ public class HorseControl : MonoBehaviour {
 	void Update () {
         GetInput();
         SetForces();
-	}
+
+
+    }
 
     void GetInput() {
         target = cam.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0)) {
-            veloc += 3f;
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            veloc += giddup;
         }
     }
 
     void SetForces() {
-        // pointer.TargetPos = new Vector2((target - pointer.ForcePoint).x, 0f);
-        Vector2 localForce = transform.InverseTransformVector(target - pointer.ForcePoint);
-        localForce = new Vector2(localForce.x, veloc);
-        pointer.TargetPos = transform.TransformVector(localForce);
-        pointer.Forces();
+        // head.TargetPos = new Vector2((target - head.ForcePoint).x, 0f);
+        Vector2 localHeadForce = transform.InverseTransformVector(target - head.ForcePoint);
+        localHeadForce = new Vector2(localHeadForce.x, 0f);
+        head.TargetPos = transform.TransformVector(localHeadForce);
+        head.Forces();
+
+
         /*
-        pointer.TargetPos = (target - pointer.ForcePoint);
+        head.TargetPos = (target - head.ForcePoint);
         Vector2 forwards = body.transform.up;
         Debug.Log(forwards);
-        pointer.TargetPos = Vector2.Scale(pointer.TargetPos, new Vector2(forwards.y, 0f));
-        pointer.Forces();
+        head.TargetPos = Vector2.Scale(head.TargetPos, new Vector2(forwards.y, 0f));
+        head.Forces();
         */
-        if (veloc > 0f)
-            veloc -= 0.1f;
+        if (veloc > 0f) {
+            bodyPointer.TargetPos = transform.up * veloc;
+            bodyPointer.Forces();
+            veloc -= 60f * Time.deltaTime;
+            Debug.Log("Veloc: " + veloc.ToString());
+        }
     }
 }
