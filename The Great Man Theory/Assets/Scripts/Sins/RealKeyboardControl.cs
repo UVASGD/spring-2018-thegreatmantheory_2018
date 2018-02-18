c7db33180871266e4ac84ccd3ca6f76d0f627b18
SimonMarkWhittle <smw2ed@virginia.edu>
2018-02-18 12:56:24 -0500

    sins comitted

---
+++ The Great Man Theory/Assets/Scripts/Sins/RealKeyboardControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealKeyboardControl : MonoBehaviour {
+
+    bool evil = true;
+
+    Rigidbody2D body;
+
+    // Use this for initialization
+    void Start() {
+        if (evil) {
+            Debug.Log("WHY ARE YOU DOING THIS PLEASE STOP");
+        }
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (evil)
+            Debug.Log("AAAAAAAAAAAAAAAA");
+
+        Vector2 mov = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        body.AddForce(mov * 5000);
+    }
+}
