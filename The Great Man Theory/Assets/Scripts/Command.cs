bfdd38f291cf78c8a264c29d57f09b9bf5ef7f70
WyotJunctor <wcj3te@virginia.edu>
2018-03-21 22:09:58 -0400

    Added Command baseclass

---
+++ The Great Man Theory/Assets/Scripts/Command.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command {
+
+    public Node subtree;
+    float timeLeft;
+
+    public Command(Node _subtree, float _timeLeft) {
+        subtree = _subtree;
+        timeLeft = _timeLeft;
+    }
+}
