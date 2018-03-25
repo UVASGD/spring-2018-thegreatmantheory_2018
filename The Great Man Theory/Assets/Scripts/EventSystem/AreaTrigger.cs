c3b5ae2fa986199a7a771bfe311d0a85d4a71972
SimonMarkWhittle <smw2ed@virginia.edu>
2018-03-25 16:02:13 -0400

    finished mousecontrol and started event system/

---
+++ The Great Man Theory/Assets/Scripts/EventSystem/AreaTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : EventTrigger {
+
+    void OnTriggerEnter2D(Collider2D other) {
+        strEvent.Invoke("");
+    }
+}
