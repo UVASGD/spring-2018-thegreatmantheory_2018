7ae519b2c43290f018b2b65e451af4ecd70667fc
SimonMarkWhittle <smw2ed@virginia.edu>
2018-03-30 19:58:22 -0400

    Level One start

---
+++ The Great Man Theory/Assets/Scripts/Level-Specific-Behaviors/GoToWaypoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToWaypoint : MonoBehaviour {
+
+    public Transform waypoint;
+    public Squad squad;
+
+    void Start() {
+        squad = GetComponent<Squad>();
+        squad.MoveCommand(waypoint.position);
+    }
+}
