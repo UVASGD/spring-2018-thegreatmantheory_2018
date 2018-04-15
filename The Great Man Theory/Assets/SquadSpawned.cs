fe866470053ba00ccb496b18629e83ccdfd792b4
SimonMarkWhittle <smw2ed@virginia.edu>
2018-04-15 16:21:04 -0400

    friendly reenforcements

---
+++ The Great Man Theory/Assets/SquadSpawned.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadSpawned : MonoBehaviour {
+
+    public SquadSpawner spawner;
+
+	void OnDestroy() {
+        spawner.SquadDeath();
+    }
+}
--- The Great Man Theory/Assets/SquadSpawner.cs
+++ The Great Man Theory/Assets/SquadSpawner.cs
@@ -5,28 +5,59 @@ using UnityEngine;
 public class SquadSpawner : MonoBehaviour {
 
     public Squad prefab;
-    List<Squad> activeSquads = new List<Squad>();
 
     public int maxSquads = 1;
+    int activeSquads = 0;
+
     public float waitTime = 0f;
     float waiting = 0f;
+
+    public SquadType squadType = SquadType.Advance;
 
     BoxCollider2D spawnArea;
 
 	// Use this for initialization
 	void Start () {
         spawnArea = GetComponent<BoxCollider2D>();
+        waiting = waitTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (activeSquads.Count < maxSquads)
+        if (activeSquads < maxSquads && GameManager.state == GameState.Gameplay)
             waiting -= Time.deltaTime;
         if (waiting <= 0f)
             MakeSquad();
 	}
 
+    public void SquadDeath() {
+        activeSquads--;
+    }
+
     void MakeSquad() {
-        return;
+        Vector3 spawnPosition;
+        if (spawnArea) {
+            Vector3 min = spawnArea.bounds.min;
+            Vector3 max = spawnArea.bounds.max;
+
+            float x = Random.Range(min.x, max.x);
+            float y = Random.Range(min.y, max.y);
+
+            spawnPosition = new Vector3(x, y, transform.position.z);
+        }
+        else
+            spawnPosition = transform.position;
+
+        Squad newSquad = Instantiate(prefab, spawnPosition, transform.rotation);
+
+        newSquad.squadType = squadType;
+        newSquad.SetDefaultBehavior(squadType);
+
+        SquadSpawned spawned = newSquad.GetComponent<SquadSpawned>();
+        if (spawned)
+            spawned.spawner = this;
+
+        activeSquads++;
+        waiting = waitTime;
     }
 }
