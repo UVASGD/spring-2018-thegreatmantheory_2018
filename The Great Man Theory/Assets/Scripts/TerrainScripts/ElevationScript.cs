6a0823e754766666dbac3e86c44b85f37350ed19
mev8vy <mev8vy@virginia.edu>
2018-04-22 14:34:58 -0400

    Elevation Script Start
    Started elevation script... definitely needs some work

---
+++ The Great Man Theory/Assets/Scripts/TerrainScripts/ElevationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevationScript : MonoBehaviour {
+    public float pub_elevation = 5;
+    private float priv_elevation = 5;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(priv_elevation-pub_elevation > 0.0)
+        {
+            priv_elevation = pub_elevation;
+            gameObject.transform.localScale(priv_elevation);
+        }
+	}
+}
--- The Great Man Theory/Assets/Scripts/TerrainScripts/SlopeScript.cs
+++ The Great Man Theory/Assets/Scripts/TerrainScripts/SlopeScript.cs
@@ -31,6 +31,6 @@ public class SlopeScript : MonoBehaviour {
 
             collider.attachedRigidbody.AddForce(slopeForce);
             //TODO SET COLLIDER ELEVATION
-        }
+                   }
     }
 }
