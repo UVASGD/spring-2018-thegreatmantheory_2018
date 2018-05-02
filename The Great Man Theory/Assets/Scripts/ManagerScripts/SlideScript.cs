be909c21aa205589e3317634dedf27c85b268e45
WyotJunctor <wcj3te@virginia.edu>
2018-05-02 13:12:03 -0400

    slide script added

--- The Great Man Theory/Assets/Scripts/ManagerScripts/SlideScript.cs
+++ The Great Man Theory/Assets/Scripts/ManagerScripts/SlideScript.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SlideScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public AudioSource aud;
+
+    bool going = false;
+
+    public SlideScript next;
+
+    public Text text;
+    public Image image;
+
+    public void Go() {
+        going = true;
+        text = GetComponentInChildren<Text>();
+        image = GetComponentInChildren<Image>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (going) {
+            if (!aud.isPlaying) {
+                if (next) {
+                    going = false;
+                    gameObject.SetActive(false);
+                    next.gameObject.SetActive(true);
+                }
+
+                else {
+                    StartCoroutine("LoadNext");
+                }
+
+            }
+        }
 	}
+
+    public IEnumerator LoadNext() {
+        while (image.color.a > 0.01) {
+            image.color = Color.Lerp(image.color, Color.clear, 0.05f);
+            text.color = Color.Lerp(text.color, Color.clear, 0.05f);
+            yield return null;
+        }
+        SceneManager.LoadScene("MainMenu");
+    }
 }
