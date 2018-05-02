f8dc18fd5abccd566e0527a7a596cd04964e54a5
WyotJunctor <wcj3te@virginia.edu>
2018-05-02 04:01:20 -0400

    Health regen added to level 2

---
+++ The Great Man Theory/Assets/Scripts/HealthRegen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthRegen : MonoBehaviour {
+
+    Body body;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Body>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (body.Health < body.maxHealth) body.Damage(5 * Time.deltaTime);
+	}
+}
